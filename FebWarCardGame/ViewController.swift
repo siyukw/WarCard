add667371cfcf4ec3b150064b7aa49654f1912ec
Siyu Wang <siyukw@D-10-157-43-205.dhcp4.washington.edu>
2017-02-11 00:51:21 -0800

    finished

--- FebWarCardGame/ViewController.swift
+++ FebWarCardGame/ViewController.swift
@@ -14,16 +14,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftCard: UIImageView!
     // score labels
     @IBOutlet weak var leftScoreLabel: UILabel!
+    var leftScore = 0;
     @IBOutlet weak var rightScoreLabel: UILabel!
+    var rightScore = 0;
 
     @IBAction func dealButton(_ sender: Any) {
-        // randomize left number from 2 to 10
-        let leftNum = arc4random_uniform(9) + 2
-        // randomize right number from 2 to 10
-        let rightNum = arc4random_uniform(9) + 2
+        // randomize left number from 2 to 14
+        let leftNum = arc4random_uniform(13) + 2
+        // randomize right number from 2 to 14
+        let rightNum = arc4random_uniform(13) + 2
         
         leftCard.image = UIImage(named: "card\(leftNum)");
         rightCard.image = UIImage(named: "card\(rightNum)");
+        
+        if leftNum > rightNum {
+            // left card wins
+            leftScore += 1;
+            leftScoreLabel.text = String(leftScore)
+        } else if leftNum == rightNum {
+            
+        } else {
+            // right card wins
+            rightScore += 1;
+            rightScoreLabel.text = String(rightScore)
+        }
     }
     
     override func viewDidLoad() {
